c40c602f265dece4794a8f2348b5975076a22284
stayflipping11 <AlfieMilo341@outlook.com>
2025-02-12 19:45:39 +0000

    Update gui.py
    finsihd it

--- gui.py
+++ gui.py
@@ -1,19 +1,29 @@
 
+
 import tkinter as tk
 from tkinter import ttk
 
 window=tk.Tk()
 window.title("buttons function")
-window.geometry("500x500")
+window.geometry("400x400")
 
 entry = tk.Entry(window, width=18, font=("Arial", 30), borderwidth=1, relief="sunken")
 entry.grid(row=0, column=0, columnspan=4)
 
+def calc():
+   try:
+      result=str(eval(entry.get()))
+      entry.delete(0,tk.END)
+      entry.insert(0,result)
+   except:
+      clear()
+      entry.insert(0,"error")
+      
 def update(value):
    current=entry.get()
    entry.delete(0,tk.END)
    entry.insert(tk.END,current + value)
-def clear(value):
+def clear():
    entry.delete(0,tk.END)
 buttons = [
    '1', '2', '3', '-',
@@ -27,11 +37,15 @@ col = 0
 
 for button in buttons:
     if button == '=':
-        tk.Button(window, text=button, width=6, height=2,command=lambda value= button: update(value), font=("Arial", 18)).grid(row=row, column=col)
-    elif button=='C':
-      tk.Button(window, text=button, width=6, height=2,command=lambda value= button: clear(value), font=("Arial", 18)).grid(row=row, column=col)
+        tk.Button(window, text=button, width=8, height=2,
+                  command=calc, font=("Arial", 15)).grid(row=row, column=col)
+    elif button == 'C':
+        tk.Button(window, text=button, width=8, height=2,
+                  command=clear, font=("Arial", 15)).grid(row=row, column=col)
     else:
-        tk.Button(window, text=button, width=6, height=2,command=lambda value= button: update(value), font=("Arial", 18)).grid(row=row, column=col)
+        tk.Button(window, text=button, width=8, height=2,
+                  command=lambda value=button: update(value), font=("Arial", 15)).grid(row=row, column=col)
+    
 
     col += 1
     if col > 3:
